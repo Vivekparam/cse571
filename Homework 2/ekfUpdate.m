f64bd3d63213da121003ac2a13584101ad07880c
Vivek Paramasivam <paramv@cs.washington.edu>
2015-10-27 14:51:59 -0700

    kalman complete

--- Homework 2/ekfUpdate.m
+++ Homework 2/ekfUpdate.m
@@ -32,156 +32,41 @@ function [mu, sigma, predMu, predSigma, zHat, pOfZ, G, V, H, K] = ...
 
 % You should almost certainly leave the header the way it is.
 
-% Init vars
-Delta_rot1 = u(1);
-Delta_trans = u(2);
-Delta_rot2 = u(3);
-Pos_prev_x = mu(1);
-Pos_prev_y = mu(2);
-Pos_prev_theta = mu(3);
-g=@prediction;
-
 % --------------------------------------------
 % Prediction step
 % --------------------------------------------
 
 % some stuff here
-predMu  = g(mu, u);
+predMu  = prediction(mu, u);
 
 % finite difference
 h = 1e-7;
-% h = 0.1;
 
 
 %% Produce G
-muDelx = [  Pos_prev_x + h
-            Pos_prev_y
-            Pos_prev_theta];
-muDely = [  Pos_prev_x
-            Pos_prev_y + h
-            Pos_prev_theta];
-
-muDeltheta = [  Pos_prev_x
-                Pos_prev_y
-                minimizedAngle(Pos_prev_theta + h)  ];
-
-predMuDelX = prediction(muDelx, u);
-predMuDelY = prediction(muDely, u);
-predMuDelTheta = prediction(muDeltheta, u);
-
-G1 = zeros(3, 1);
-G2 = zeros(3, 1);
-G3 = zeros(3, 1);
-
-% first col, changing x
-for i = 1:2
-    n_0 = predMu(i);
-    n_1 = predMuDelX(i);
-    dn = n_1 - n_0;
-    G1(i) = dn / h;
+G = zeros(3, 3);
+for i = 1:3
+    temp = mu;
+    temp(i) = temp(i) + h;
+    predMuDel = prediction(temp, u);
+    G(:, i) = predMuDel - predMu;
+    %G(3, i) = minimizedAngle(G(3, i));
 end
 
-theta_0 = predMu(3);
-theta_1 = predMuDelX(3);
-dtheta = minimizedAngle(theta_1 - theta_0);
-
-G1(3) = dtheta / h;
-  
-% second col, changing y
-for i = 1:2
-    n_0 = predMu(i);
-    n_1 = predMuDelY(i);
-    dn = n_1 - n_0;
-    G2(i) = dn / h;
-end
-
-theta_0 = predMu(3);
-theta_1 = predMuDelY(3);
-dtheta = minimizedAngle(theta_1 - theta_0);
-
-G2(3) = dtheta / h;
-
-% third col, changing theta
-for i = 1:2
-    n_0 = predMu(i);
-    n_1 = predMuDelTheta(i);
-    dn = n_1 - n_0;
-    G3(i) = dn / h;
-end
-
-theta_0 = predMu(3);
-theta_1 = predMuDelTheta(3);
-dtheta = minimizedAngle(theta_1 - theta_0);
-
-G3(3) = dtheta / h;
-
-G = horzcat(G1, G2);
-G = horzcat(G, G3);
-
+G = G / h;
 
 %% Produce V
-V1 = zeros(3, 1);
-V2 = zeros(3, 1);
-V3 = zeros(3, 1);
+V = zeros(3, 3);
 
-uDelx = [minimizedAngle(Delta_rot1 + h)
-         Delta_trans
-         Delta_rot2];
-uDely = [Delta_rot1
-         Delta_trans + h
-         Delta_rot2];
-uDeltheta = [Delta_rot1
-         Delta_trans
-         minimizedAngle(Delta_rot2 + h)];
-preduDelX = prediction(mu, uDelx);
-preduDelY = prediction(mu, uDely);
-preduDelTheta = prediction(mu, uDeltheta);
-
-% first col, changing x
-for i = 1:2
-    n_0 = predMu(i);
-    n_1 = preduDelX(i);
-    dn = n_1 - n_0;
-    V1(i) = dn / h;
-end
-theta_0 = predMu(3);
-theta_1 = preduDelX(3);
-dtheta = minimizedAngle(theta_1 - theta_0);
-
-V1(3) = dtheta / h;
-
-% second col, changing y control
-for i = [1:2]
-    n_0 = predMu(i);
-    n_1 = preduDelY(i);
-    dn = n_1 - n_0;
-    V2(i) = dn / h;
-end
-theta_0 = predMu(3);
-theta_1 = preduDelY(3);
-
-dtheta = minimizedAngle(theta_1 - theta_0);
-
-V2(3) = dtheta / h;
-  
-  
-% third col, changing theta control
-for i = [1:2]
-    n_0 = predMu(i);
-    n_1 = preduDelTheta(i);
-    dn = n_1 - n_0;
-    V3(i) = dn / h;
+for i = 1:3
+    temp = u;
+    temp(i) = temp(i) + h;
+    predMuDel = prediction(mu, temp);
+    V(:, i) = predMuDel - predMu;
+    %V(3, i) = minimizedAngle(V(3, i));
 end
 
-theta_0 = predMu(3);
-theta_1 = preduDelTheta(3);
-dtheta = minimizedAngle(theta_1 - theta_0);
-
-V3(3) = dtheta / h;
-
-% Combine to form V
-V = horzcat(V1, V2);
-V = horzcat(V, V3);
+V = V / h;
 
 %% Produce SigmaHat
 M_t = diag(noiseFromMotion(u, filterAlphas));
@@ -194,26 +79,19 @@ predSigma = sigmaHat;
 
 %% Compute expected observation and Jacobian
 
-zHat = observation(mu, FIELDINFO, markerId);
+zHat = observation(predMu, FIELDINFO, markerId);
 
-zHatDelx = observation(muDelx, FIELDINFO, markerId);
-zHatDely = observation(muDely, FIELDINFO, markerId);
-zHatDeltheta = observation(muDeltheta, FIELDINFO, markerId);
+H = zeros(1, 3);
 
-b_0 = zHat(1);
-b_1_Delx = zHatDelx(1);
-b_1_Dely = zHatDely(1);
-b_1_Deltheta = zHatDeltheta(1);
+for i = 1:3
+   temp = predMu;
+   temp(i) = temp(i) + h;
+   predZHatDel = observation(temp, FIELDINFO, markerId);
+   H(i) = minimizedAngle(predZHatDel(1) - zHat(1));
+end
 
+H = H / h;
 
-delbDelx = minimizedAngle(b_1_Delx - b_0) / h;
-delbDely = minimizedAngle(b_1_Dely - b_0) / h;
-delbDeltheta = minimizedAngle(b_1_Deltheta - b_0) / h;
-
-H = [ delbDelx delbDely delbDeltheta ];
-
-Q =  [  [filterBeta(1).^ 2 0
-            0               0]  ];
 Q = filterBeta(1);
 
 S = H * sigmaHat * transpose(H) + Q;
@@ -237,40 +115,8 @@ K = sigmaHat * transpose(H) * inv(S);
 mu = predMu + K * innovation;
 I = eye(3);
 sigma = (I - K * H) * sigmaHat;
-% sigma = sigmaHat;
-% mu = predMu;
 
-pOfZ = sqrt(det(2 * pi * S)) * exp(-0.5 * transpose(innovation) * inv(S) * innovation);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pOfZ = likelihood(S, innovation); %sqrt(det(2 * pi * S)) * exp(-0.5 * transpose(innovation) * inv(S) * innovation);
 
 
 
